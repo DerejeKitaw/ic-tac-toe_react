d9fdf2409152a534018a326cc83181d0cbe921cb
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-08-25 23:34:29 -0400

    Taking Turns

--- src/index.js
+++ src/index.js
@@ -14,12 +14,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     squares[i] = 'X';
-    this.setState({squares: squares});
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   renderSquare(i) {
     return (
@@ -31,8 +36,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
         <div className="status">{status}</div>
